313f8a8cdf9d6a2446c42bbfff5ae7704e5ad6df
lxinhui <miaoxinhuili@gmail.com>
2019-07-26 18:21:02 -0700

    add end scripts

---
+++ scripts/enb_end.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+screen_ue="ue"
+screen -S $screen_ue -X quit
+
+screen_enb="enb"
+screen -S $screen_enb -X quit
---
+++ scripts/epc_end.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+screen_u="spgwu"
+screen -S $screen_u -X quit
+
+screen_c="spgwc"
+screen -S $screen_c -X quit
+
+screen_mme="mme"
+screen -S $screen_mme -X quit
+
+screen_hss="hss"
+screen -S $screen_hss -X quit
